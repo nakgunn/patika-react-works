12b830bfd10e996ecc26399657ed10f112509d88
Necati Akgün <necatiakgun97@gmail.com>
2022-10-12 15:22:48 +0300

    init

---
+++ giris/app.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+async function getData(user_id) {
+    const {data: user} = await axios('https://jsonplaceholder.typicode.com/users/' + user_id )
+    const {data: posts} = await axios('https://jsonplaceholder.typicode.com/posts?userId=' + user_id )
+    user.posts = posts;
+    return user;
+}
+
+export default getData
---
+++ giris/index.js
@@ -0,0 +1,4 @@
+import getData from "./app.js"
+
+const data = await getData(1);
+console.log(data);
